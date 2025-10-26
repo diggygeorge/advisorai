8a1cc1b2d8da43fa728ea27a74ed2efdeac1fc5b
yewon-yun <yunyw@bu.edu>
2025-10-25 20:48:07 -0400

    updated page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -27,6 +27,7 @@ const playfair = Playfair_Display({
 export default function Dashboard() {
   const [major, setMajor] = useState('')
   const [career, setCareer] = useState('')
+  const [school, setSchool] = useState('')
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
@@ -43,7 +44,7 @@ export default function Dashboard() {
         <form action={signOut}>
           <button
             type="submit"
-            className={`${inter.className} rounded-md bg-white px-4 py-2 text-slate-700 hover:bg-slate-200 transition`}
+            className={`${inter.className} rounded-md bg-white px-4 py-2 text-slate-700 hover:bg-slate-200 cursor-pointer transition`}
           >
             Sign Out
           </button>
@@ -51,29 +52,51 @@ export default function Dashboard() {
       </nav>
 
       {/* Main Content */}
-      <main className="flex flex-col items-center justify-center mt-20 px-4">
-      <h1 className={`${playfair.className} text-slate-900 text-4xl font-semibold mb-6`}>Plan Your Path</h1>
+      <main className="flex flex-col items-center justify-center mt-40 px-4">
+      <h1 className={`${playfair.className} text-slate-900 text-4xl font-semibold mb-10`}>
+        Plan Your Path</h1>
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col gap-4 w-full max-w-md bg-white p-6 rounded-lg shadow-md"
+          className="flex flex-col gap-6 w-full max-w-2xl"
         >
+          <div className="flex w-full gap-4">
+          
+
+          <select
+          value={school}
+          onChange={(e) => setSchool(e.target.value)}
+          className="flex-[2] px-4 py-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white cursor-pointer">
+          <option value="">Select School</option>
+          <option value="CAS">CAS</option>
+          <option value="CAS">CFA</option>
+          <option value="CAS">COM</option>
+          <option value="CAS">QST</option>
+          <option value="CAS">SAR</option>
+          <option value="CFA">ENG</option>
+          <option value="CFA">WHE</option>
+          <option value="QST">MET</option>
+          </select>
+
+
           <input
             type="text"
             placeholder="Enter your major"
             value={major}
             onChange={(e) => setMajor(e.target.value)}
-            className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="px-4 py-3 border rounded-md w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          </div>
+
           <input
             type="text"
             placeholder="Enter your career path"
             value={career}
             onChange={(e) => setCareer(e.target.value)}
-            className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="px-4 py-3 border rounded-md w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <button
             type="submit"
-            className="px-4 py-2 bg-slate-700 text-white rounded-md hover:bg-slate-900 transition"
+            className="px-4 py-2 bg-slate-700 text-white rounded-md hover:bg-slate-900 cursor-pointer transition"
           >
             OK
           </button>
